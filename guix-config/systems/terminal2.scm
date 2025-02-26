3a60547e050ca7fa18d793b039cb7e93c410508c
Jaesung Lee <ecumenepolis@gmail.com>
2025-02-27 08:54:27 +0900

    add bootloader and filesystem info to base config

--- guix-config/systems/terminal2.scm
+++ guix-config/systems/terminal2.scm
@@ -14,6 +14,7 @@
 (use-modules (gnu) (nongnu packages linux) (guix))
 (use-service-modules cups desktop networking ssh xorg)
 
+;; Not meant to be used on its own.
 (define base-system
   (operating-system
     (host-name "base-system")
@@ -43,9 +44,25 @@
         ;; To configure OpenSSH, pass an 'openssh-configuration'
         ;; record as a second argument to 'service' below.
         (service openssh-service-type))
-      ;; This is the default list of services we
-      ;; are appending to.
-      %base-services))))
+    ;; This is the default list of services we
+    ;; are appending to.
+    %base-services))
+    (bootloader (bootloader-configuration
+              (bootloader grub-bootloader)
+              (targets (list "/dev/sdb"))
+              (keyboard-layout (operating-system-keyboard-layout terminal-system))))
+
+    ;; The list of file systems that get "mounted".  The unique
+    ;; file system identifiers there ("UUIDs") can be obtained
+    ;; by running 'blkid' in a terminal.
+    (file-systems (cons* (file-system
+                          (mount-point "/")
+                          (device (uuid
+                                    "e45a594e-0627-4979-9751-b3f33c9989f5"
+                                    'ext4))
+                          (type "ext4")) %base-file-systems))))
+
+
 
 ;; (define server)
 
