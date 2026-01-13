7eb338cf710c1bb998a20ffc96aec9d7f3314ec5
Jaesung Lee <ljaesung1130@gmail.com>
2026-01-13 11:27:01 +0900

    Fixed channels.scm to point to this repo too

--- channels.scm
+++ channels.scm
@@ -15,4 +15,8 @@
           (make-channel-introduction
             "897c1a470da759236cc11798f4e0a5f7d4d59fbc"
             (openpgp-fingerprint
-              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5")))))
+              "2A39 3FFF 68F4 EF7A 3D29  12AF 6F51 20A0 22FB B2D5"))))
+      (channel
+	(name 'my-setup) ;; You can name this whatever you want
+	(url "file:///home/user0/projects/public/guix-setup") ;; Path to repo root
+	(branch "master")))
